df73d017b29e6b2836158b60862490ecb8b73cec
Ümüd Məmmədov <65266946+umudmmmdov1@users.noreply.github.com>
2020-11-23 20:09:57 +0400

    Update __main__.py

--- dto_installer/__main__.py
+++ dto_installer/__main__.py
@@ -145,7 +145,7 @@ if __name__ == "__main__":
     config['TMP_DOWNLOAD_DIRECTORY'] = "./downloads/"
     config['TZ'] = TZ
     config['TZ_NUMBER'] = "1"
-    config['UPSTREAM_REPO_URL'] = "https://github.com/umudmmmdov1DTOUserBot"
+    config['UPSTREAM_REPO_URL'] = "https://github.com/umudmmmdov1/DTOUserBot"
     config['WARN_LIMIT'] = "3"
     config['WARN_MODE'] = "gmute"
     config['LANGUAGE'] = LANGUAGE
